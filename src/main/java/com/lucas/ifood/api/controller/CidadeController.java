2858d90dc7768d5f33e9f49139804396700cb945
LucasEscopelli <67751512+LucasEscopelli@users.noreply.github.com>
2023-05-05 01:52:04 -0300

    adicionando

--- src/main/java/com/lucas/ifood/api/controller/CidadeController.java
+++ src/main/java/com/lucas/ifood/api/controller/CidadeController.java
@@ -3,21 +3,18 @@ package com.lucas.ifood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.lucas.ifood.domain.model.Cidade;
 import com.lucas.ifood.domain.repository.CidadeRepository;
 
-@RestController
-@RequestMapping("/cidades")
+//@RestController
+//@RequestMapping("/cidades")
 public class CidadeController {
 
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
-	@GetMapping
+//	
+//	@GetMapping
 	public List<Cidade> listar(){
 		return cidadeRepository.listar();
 	}
